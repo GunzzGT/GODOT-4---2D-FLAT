3645c87c0aca3cca10c47c3d064b2ca6a04f69f8
GunzzGT <gunawantandy@gmail.com>
2023-05-16 18:30:55 +0700

    Update CharacterBody2D.gd

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -13,18 +13,26 @@ func _physics_process(_delta):
 	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	
 	# look_at(get_global_mouse_position())
+	# look_at actually works to alter the entire node direction, "right" or 270 degree angle is the forward by default
 
 	if direction:
 		velocity = direction * SPEED
+		
+		# scuffed function to determine sprite rotation
+		# not direction because I don't know how to define which is forward or backwards from the node
 		if velocity.x != 0:
-			$Sprite2D.flip_v = false
+			# vertical flip false should not be needed here actually ($Sprite2D.flip_v = false) deleted
 			if velocity.x < 0:
 				$Sprite2D.rotation_degrees = 90
 			else:
 				$Sprite2D.rotation_degrees = 270
+				# since horizontal flip does not make notable difference (sprite shape kind of mirrored vertically)
+				# if less than 0 = true means left and vice versa
 		elif velocity.y != 0:
 			$Sprite2D.rotation_degrees = 0
+			# set rotation back to neutral
 			$Sprite2D.flip_v = velocity.y > 0
+			# flip vertically, for now decide that the top of the sprite (top of the head) is "forward" in sprite only, not actual usable direction
 		
 	else:
 		velocity = Vector2.ZERO
