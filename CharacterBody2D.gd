0505e52378db2f8cb5e44dc2eed35090801a4486
GunzzGT <gunawantandy@gmail.com>
2023-05-16 16:52:44 +0700

    Added sprite direction
    Added rotating sprite based on Vector2, trying to figure out how node direction works but getting nowhere on the forums and tutorials. Scuffed direction by rotating sprite for now.

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,23 +1,33 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP = -400.0 # not used
 
 # no gravity, no jumping yet
 # delta can also be understood as "on button press hold"
 # move_toward is fancy Vector * 0
 
-func _physics_process(delta):
-
+func _physics_process(_delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	
+	# look_at(get_global_mouse_position())
+
 	if direction:
 		velocity = direction * SPEED
+		if velocity.x != 0:
+			$Sprite2D.flip_v = false
+			if velocity.x < 0:
+				$Sprite2D.rotation_degrees = 90
+			else:
+				$Sprite2D.rotation_degrees = 270
+		elif velocity.y != 0:
+			$Sprite2D.rotation_degrees = 0
+			$Sprite2D.flip_v = velocity.y > 0
+		
 	else:
-		velocity = Vector2(0,0)
+		velocity = Vector2.ZERO
 		# velocity.x = move_toward(velocity.x, 0, SPEED)
 		# velocity.y = move_toward(velocity.y, 0, SPEED)
 		# velocity.x = 0 and velocity.y = 0 also works
@@ -25,3 +35,5 @@ func _physics_process(delta):
 		# velocity = Vector2(0,0)
 
 	move_and_slide()
+	
+
